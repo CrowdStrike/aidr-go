bb7dff19e0331959c9a2e78408532520ffc56f7a
Kenan Yildirim <kenan.yildirim@crowdstrike.com>
2026-01-05 13:23:31 -0500

    feat: support Go v1.24 (#6)

---
+++ devenv.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  languages.go = {
+    enable = true;
+    version = "1.25.5";
+  };
+  starship.enable = true;
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/crowdstrike/aidr-go
 
-go 1.25
+go 1.24
+
+toolchain go1.25.5
 
 require (
 	github.com/tidwall/gjson v1.18.0
